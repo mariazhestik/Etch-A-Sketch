5d33ca8b4c9f58b21d154d9dac0bc2178259a49e
mariazhestik <mariazhestik@gmail.com>
2025-03-24 11:46:43 +0000

    add create grid function

---
+++ javascript.js
@@ -0,0 +1,18 @@
+const container = document.querySelector('.container');
+const gridSize = 16;
+
+function createGrid(size) {
+    container.innerHTML = '';
+    const squareSize = 960 / size;
+
+    for (let i = 0; i < size; i++) {
+        const square = document.createElement('div');
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+        square.classList.add('grid-square');
+
+        container.appendChild(square);
+    }
+}
+
+createGrid(gridSize);
