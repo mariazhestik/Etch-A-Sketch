871c0fe4201466d9222064518044a494ad015f69
mariazhestik <mariazhestik@gmail.com>
2025-03-24 11:55:35 +0000

    fixed create grid function

--- javascript.js
+++ javascript.js
@@ -3,12 +3,15 @@ const gridSize = 16;
 
 function createGrid(size) {
     container.innerHTML = '';
-    const squareSize = 960 / size;
+
+    container.display = 'grid';
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr`;
+    container.style.width = `${squareSize}px`;
+    container.style.height = `${squareSize}px`;
 
     for (let i = 0; i < size * size; i++) {
         const square = document.createElement('div');
-        square.style.width = `${squareSize}px`;
-        square.style.height = `${squareSize}px`;
         square.classList.add('grid-square');
 
         container.appendChild(square);
